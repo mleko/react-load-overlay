e38bb92597e7c9974696ee2903b47ba744ef4410
Daniel Król <daniel@krol.me>
2017-01-27 19:51:31 +0100

    Position spinner in the middle of visible part of overlay

--- src/LoadingOverlay/index.tsx
+++ src/LoadingOverlay/index.tsx
@@ -6,6 +6,10 @@ interface Props {
 
 export class LoadingOverlay extends React.Component<Props, void> {
 
+	private spinner: HTMLDivElement;
+	private overlay: HTMLDivElement;
+	private container: HTMLDivElement;
+
 	public render(): JSX.Element {
 		const overlayStyle = {
 			position: "absolute",
@@ -16,21 +20,61 @@ export class LoadingOverlay extends React.Component<Props, void> {
 			backgroundColor: "rgba(150,150,150,0.8)",
 			zIndex: 9999,
 			display: this.props.loading ? "block" : "none",
+			transition: "padding 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275)"
 		};
 		const containerStyle = {
 			position: "relative"
 		};
 		return (
-			<div style={containerStyle}>
+			<div style={containerStyle} ref={this.mountContainer}>
 				{this.props.children}
 				<div
+					ref={this.mountOverlay}
 					style={overlayStyle}
 				>
-					<div style={{textAlign: "center"}}>
+					<div ref={this.mountSpinner} style={{textAlign: "center"}}>
 						Loading
 					</div>
 				</div>
 			</div>
 		);
+	}
+
+	public componentDidMount() {
+		document.addEventListener("scroll", this.positionSpinner);
+	}
+
+	private mountSpinner = (ref: HTMLDivElement) => {
+		this.spinner = ref;
+	};
+	private mountContainer = (ref: HTMLDivElement) => {
+		this.container = ref;
+	};
+	private mountOverlay = (ref: HTMLDivElement) => {
+		this.overlay = ref;
+	};
+
+	private positionSpinner = () => {
+		const viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+		const spinnerHeight = this.spinner.offsetHeight;
+		const boundingClientRect = this.container.getBoundingClientRect();
+
+		const visiblePartY = this.getVisiblePart(viewportHeight, boundingClientRect.top, boundingClientRect.height);
+		if (null !== visiblePartY) {
+			const visiblePartYSize = visiblePartY.to - visiblePartY.from;
+			const offsetTop = Math.min(boundingClientRect.height - spinnerHeight, visiblePartY.from + (visiblePartYSize - spinnerHeight) / 2);
+			this.overlay.style.paddingTop = String(offsetTop) + "px";
+		}
+
+	};
+
+	private getVisiblePart(viewportSize, from, size) {
+		if (from >= viewportSize || from + size <= 0) {
+			return null;
+		}
+		return {
+			from: from < 0 ? -from : 0,
+			to: from + size > viewportSize ? viewportSize - from : size
+		};
 	}
 }
